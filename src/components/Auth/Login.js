e597a264cfb25ca9fad1838d03305d7e03995ea1
Richard <35225988+RichardMarkRoss@users.noreply.github.com>
2023-09-13 15:37:13 +0200

    file setup

---
+++ src/components/Auth/Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
